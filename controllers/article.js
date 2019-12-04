e9407a3900b71c4b61463a56c664b0ed740c68e7
matoeltiempo <matoeltiempoav@gmail.com>
2019-12-04 06:28:27 +0300

    change

--- controllers/article.js
+++ controllers/article.js
@@ -1,5 +1,5 @@
 const Article = require('../models/article');
-const NotDataAccess = require('../errors/not-data-access');
+const NotUserAuthorization = require('../errors/not-user-authorization');
 const NoRemove = require('../config/message');
 
 module.exports.createArticle = (req, res, next) => {
@@ -20,7 +20,7 @@ module.exports.deleteArticle = (req, res, next) => {
     .then((article) => {
       const articleData = { data: article };
       if (JSON.stringify(articleData.data.owner) !== JSON.stringify(req.user._id)) {
-        throw new NotDataAccess(NoRemove);
+        throw new NotUserAuthorization(NoRemove);
       } else {
         Article.findByIdAndRemove(req.params.articleId)
           .then(() => res.send(articleData))
